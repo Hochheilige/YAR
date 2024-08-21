1fb594d42929a52ae92aea87284d2966755b53c2
Hochheilige <hochheilige01@gmail.com>
2024-08-21 13:19:02 +0800

    replace typedef function declaration with using and add macro to declare render functions

--- source/engine/render.h
+++ source/engine/render.h
@@ -4,7 +4,26 @@
 //            Render Structures            //
 // ======================================= //
 
-// Maybe sturcts should be in basic file
+/*
+    Plans to add:
+        - Render Pipeline that should include:
+            - Blend state
+            - Rasterization state
+            - Depth|Stencil state
+            - Input Assembly 
+            - Vertex Input
+            - Viewport|Scissor
+            - Multisampling (?)
+        - Shader Reflection
+            - Need to add spirv dependency to make it
+            - want to use it 
+        - Buffers and Textures
+            - If we speak about uniforms there probably should be
+                a thing like DescriptorSet
+            - Another thing - Vertex and Index buffers
+        - Command Queue 
+            - as far as OpenGL render immediately 
+*/
 struct SwapChain
 {
     bool vsync;
@@ -16,7 +35,10 @@ struct SwapChain
 //            Render Functions             //
 // ======================================= //
 
-typedef void (*add_swapchain_fn)(SwapChain* swapchain, bool vsync);
-extern add_swapchain_fn add_swapchain;
+#define DECLARE_YAR_RENDER_FUNC(ret, name, ...) \
+using name##_fn = ret(*)(__VA_ARGS__);          \
+extern name##_fn name;                          \
+
+DECLARE_YAR_RENDER_FUNC(void, add_swapchain, SwapChain* swapchain, bool vsync);
 
 void init_render();
