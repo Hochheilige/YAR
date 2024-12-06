4accfa041571efafcec2605bd75c71625ffe9232
Hochheilige <hochheilige01@gmail.com>
2024-12-07 01:30:35 +0800

    render sphere normals

--- source/shaders/raytracing/raytracing_comp.hlsl
+++ source/shaders/raytracing/raytracing_comp.hlsl
@@ -12,28 +12,35 @@ struct ray
     float3 direction;
 };
 
-bool hit_sphere(const float3 center, double radius, const ray r) {
+float3 ray_at(const ray r, const float t)
+{
+    return r.origin + t * r.direction;
+}
+
+float hit_sphere(const float3 center, float radius, const ray r) {
     float3 oc = center - r.origin;
     float a = dot(r.direction, r.direction);
     float b = -2.0 * dot(r.direction, oc);
     float c = dot(oc, oc) - radius*radius;
     float discriminant = b*b - 4*a*c;
-    return (discriminant >= 0);
-}
 
-float3 unit_vector(float3 v)
-{
-    return v / length(v);
+    if (discriminant < 0)
+        return -1.0f;
+    else
+        return (-b - sqrt(discriminant)) / (2.0f * a);
 }
 
 float3 ray_color(const ray r)
 {
-    if (hit_sphere(float3(0.0f, 0.0f, -1.0f), 0.5f, r))
+    float3 sphere_center = float3(0.0f, 0.0f, -1.0f);
+    float t = hit_sphere(sphere_center, 0.5f, r); 
+    if (t > 0.0f)
     {
-        return float3(1.0f, 0.0f, 0.0f);
+        float3 normal = normalize(ray_at(r, t) - sphere_center);
+        return 0.5f * (normal + 1);
     }
 
-    float3 unit_direction = unit_vector(r.direction);
+    float3 unit_direction = normalize(r.direction);
     float a = 0.5*(unit_direction.y + 1.0);
     return lerp(a, float3(1.0f, 1.0f, 1.0f), float3(0.5f, 0.7f, 1.0f));
 }
@@ -45,9 +52,7 @@ void main(uint3 dispatchThreadID : SV_DispatchThreadID, uint3 groupThreadID : SV
     uint height;
     quad_tex.GetDimensions(width, height);
 
-    float2 ndc = (float2(dispatchThreadID.xy) / float2(width, height)) * 2.0f - 1.0f;
-    ndc.y *= -1.0f; 
-
+    float2 ndc = (float2(dispatchThreadID.xy) / float2(width, height)) * 2.0f - 1.0f; 
     float4 clipSpace = float4(ndc, 0.0f, 1.0f);
 
     float4 worldSpace = mul(invViewProj, clipSpace);
@@ -56,7 +61,7 @@ void main(uint3 dispatchThreadID : SV_DispatchThreadID, uint3 groupThreadID : SV
     ray r;
     r.direction = rayDir;
     r.origin = camera_pos.xyz;
-    float3 rayColor = ray_color(r);
 
+    float3 rayColor = ray_color(r);
     quad_tex[dispatchThreadID.xy] = float4(rayColor, 1.0f);
 }
