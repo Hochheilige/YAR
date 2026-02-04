60d339d8318379f55093e48bc389d408d070064c
Hochheilige <hochheilige01@gmail.com>
2026-02-04 17:44:56 +0800

    add layout generation for vertex

--- source/engine/vertex.h
+++ source/engine/vertex.h
@@ -3,6 +3,7 @@
 #include "math/vector2.h"
 #include "math/vector3.h"
 #include "math/vector4.h"
+#include "render.h"
 
 struct VertexStatic
 {
@@ -12,6 +13,8 @@ struct VertexStatic
 	Vector3 bitangent;
 	Vector2 uv0;
 	Vector2 uv1;
+
+	static void setup_layout(yar_vertex_layout& layout);
 };
 
 struct VertexUnlit
@@ -19,9 +22,13 @@ struct VertexUnlit
 	Vector3 position;
 	Vector2 uv;
 	Vector4 color;
+
+	static void setup_layout(yar_vertex_layout& layout);
 };
 
 struct VertexSkybox
 {
 	Vector3 position;
+
+	static void setup_layout(yar_vertex_layout& layout);
 };
