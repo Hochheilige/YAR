9cb4fb2f0806ba45e061beb3a8f806af572c6860
Hochheilige <hochheilige01@gmail.com>
2026-02-04 23:04:54 +0800

    fix wrong vertex layout init

--- source/engine/vertex.cpp
+++ source/engine/vertex.cpp
@@ -5,24 +5,24 @@
 void VertexStatic::setup_layout(yar_vertex_layout& layout)
 {
 	layout.attrib_count = 6u;
-	layout.attribs[0] = { 3u, yar_attrib_format_float, offsetof(VertexStatic, position) };
-	layout.attribs[1] = { 3u, yar_attrib_format_float, offsetof(VertexStatic, normal) };
-	layout.attribs[2] = { 3u, yar_attrib_format_float, offsetof(VertexStatic, tangent) };
-	layout.attribs[3] = { 3u, yar_attrib_format_float, offsetof(VertexStatic, bitangent) };
-	layout.attribs[4] = { 2u, yar_attrib_format_float, offsetof(VertexStatic, uv0) };
-	layout.attribs[5] = { 2u, yar_attrib_format_float, offsetof(VertexStatic, uv1) };
+	layout.attribs[0] = { 3u, yar_attrib_format_float, 0, offsetof(VertexStatic, position) };
+	layout.attribs[1] = { 3u, yar_attrib_format_float, 0, offsetof(VertexStatic, normal) };
+	layout.attribs[2] = { 3u, yar_attrib_format_float, 0, offsetof(VertexStatic, tangent) };
+	layout.attribs[3] = { 3u, yar_attrib_format_float, 0, offsetof(VertexStatic, bitangent) };
+	layout.attribs[4] = { 2u, yar_attrib_format_float, 0, offsetof(VertexStatic, uv0) };
+	layout.attribs[5] = { 2u, yar_attrib_format_float, 0, offsetof(VertexStatic, uv1) };
 }
 
 void VertexUnlit::setup_layout(yar_vertex_layout& layout)
 {
 	layout.attrib_count = 3u;
-	layout.attribs[0] = { 3u, yar_attrib_format_float, offsetof(VertexUnlit, position) };
-	layout.attribs[1] = { 2u, yar_attrib_format_float, offsetof(VertexUnlit, uv) };
-	layout.attribs[2] = { 4u, yar_attrib_format_float, offsetof(VertexUnlit, color) };
+	layout.attribs[0] = { 3u, yar_attrib_format_float, 0, offsetof(VertexUnlit, position) };
+	layout.attribs[1] = { 2u, yar_attrib_format_float, 0, offsetof(VertexUnlit, uv) };
+	layout.attribs[2] = { 4u, yar_attrib_format_float, 0, offsetof(VertexUnlit, color) };
 }
 
 void VertexSkybox::setup_layout(yar_vertex_layout& layout)
 {
 	layout.attrib_count = 1u;
-	layout.attribs[0] = { 3u, yar_attrib_format_float, offsetof(VertexSkybox, position) };
+	layout.attribs[0] = { 3u, yar_attrib_format_float, 0, offsetof(VertexSkybox, position) };
 }
